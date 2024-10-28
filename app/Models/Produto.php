6e63c615ec2d3b31828f09429f379101fc702171
Kamily Santiago <santiagokamily87@gmail.com>
2024-10-28 19:28:41 -0300

    primeiro commit

---
+++ app/Models/Produto.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Models;
+
+use Carbon\Traits\Timestamp;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Produto extends Model {
+    use SoftDeletes, Timestamp;
+    protected $table = 'produto';
+}
